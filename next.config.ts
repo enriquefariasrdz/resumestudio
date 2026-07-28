3edf4165754de2ff77433ed5a8cf5fc5d5bfc773
Enrique Farias <enriquefariasrdz@gmail.com>
2026-07-27 22:22:14 -0600

    Improving OCR, still having issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pdf-parse-debugging-disabled', 'mammoth'],
+  serverExternalPackages: [
+    'pdf-parse-debugging-disabled',
+    'mammoth',
+    'pdfjs-dist',
+    'tesseract.js',
+    '@napi-rs/canvas',
+  ],
 };
 
 export default nextConfig;
